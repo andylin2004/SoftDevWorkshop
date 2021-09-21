9a722f102643d0086eceff26294a24fd2dc4bf08
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-21 09:24:20 -0400

    made the name generator file (slight non collab version)

---
+++ NameGenerator/nameGenerator.py
@@ -0,0 +1,15 @@
+from random import randrange
+
+def openNameFile():
+    file = open("nameFile.txt", "r")
+    fileLines = []
+    for line in file:
+        fileLines.append(line)
+    return fileLines
+
+fileLines = openNameFile()
+input = input("Input a number for a student, or enter for a random")
+if input.isnumeric and input != "0":
+    print(fileLines[int(input)])
+else:
+    print(fileLines[randrange(fileLines.count)])
