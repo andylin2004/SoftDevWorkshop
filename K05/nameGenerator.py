dacb3869607488df3879ee25de6d47fe7c1ee895
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-27 09:24:36 -0400

    added what code does and trio discussion notes

--- K05/nameGenerator.py
+++ K05/nameGenerator.py
@@ -3,8 +3,22 @@
 # K05 - combining code between trios to make a better student RNG selector
 # 2021-09-27
 
+# Summary of trio discussion
+# We discussed about how to combine code together and learning how each other's code worked and learned what was the good parts of each code (my input selector, file structure of Jonathan's, and Wenhao's random choice thing)
+
+# Discoveries
+# We found out that we could probably just use random.choice when we are selecting a random item from a dictionary, as well as building the database within the file instead of accessing an outside file.
+
+# Questions
+# No questions at all.
+
+# Comments
+# We should've used GitHub to get all the students names instead of using placeholder names
+
+# get random library
 import random
 
+# class list for both periods
 crews = {
     'pd1': ['Owen Yaggy', 'Haotain Gan', 'Ishraq Mahid', 'Julia Nelson', 'Ivan Lam',
        'Michelle Lo', 'Christopher Liu', 'Michael Borzuk', 'Ivan Mijacika',
@@ -22,12 +36,15 @@ crews = {
        'Kevin Cao', 'Sophie Liu', 'Annabel Zhang', 'Roshani Shrestha', 'Han Zhang']
 }
 
-
+# get input of what period the user wants to get random person
 input = input("Input period number (1 or 2)")
-if input.isnumeric() and input != "0":
-    if input == "1":
-        print(random.choice(crews.pd1))
-    elif input == "2":
-        print(random.choice(crews.pd2))
-    else:
-        print("invalid period--please try again")
+# see if period 1 or 2, or invalid input
+if input == "1":
+    # get random person from period 1
+    print(random.choice(crews['pd1']))
+elif input == "2":
+    # get random person from period 2
+    print(random.choice(crews['pd2']))
+else:
+    # tell user they put invalid option
+    print("invalid period--please try again")
