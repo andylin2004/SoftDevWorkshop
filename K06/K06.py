b7211ac2283220bd41e06508aa0cac8ff4a62814
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-28 14:28:46 -0400

    made the weighted probability thing work

--- K06/K06.py
+++ K06/K06.py
@@ -3,8 +3,22 @@ import random
 file = open("occupations.csv", "r")
 occupations = {}
 for line in file:
-    newLine = line.strip()
-    splitLine = newLine.split(",")
-    occupations[splitLine[0]] = splitLine[1]
+    line = line.strip()
+    splitLine = []
+    quotes = False
+    samplingLine = ""
+    for char in line:
+        if char == '"':
+            quotes = not quotes
+        elif char == ',' and not quotes:
+            splitLine.append(samplingLine)
+            samplingLine = ""
+        else:
+            samplingLine += char
+    splitLine.append(samplingLine)
+    if splitLine[1] != "Percentage" and splitLine[0] != "Total":
+        occupations[splitLine[0]] = float(splitLine[1])
 
-print(occupations)
+# for some reason python freaks out when I force this into the choices thing
+keys = list(occupations.keys())
+print(random.choices(keys, weights=occupations.values(), k=1)[0])
