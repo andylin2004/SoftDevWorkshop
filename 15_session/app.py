5a29a9c9ea2cb90017e3992eda5fda83db45ad82
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-19 09:44:12 -0400

    more robust login handling and check to see if user is logged in

--- 15_session/app.py
+++ 15_session/app.py
@@ -6,12 +6,15 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+from flask import session           #facilitate user sessions
+from os import urandom
 
 app = Flask(__name__)    #create Flask object
 
-
 @app.route("/")
 def disp_loginpage():
+    if "username" in session:
+        return render_template('response.html', username=session["username"])
     return render_template( 'login.html' ) # Render the login template
 
 
@@ -20,19 +23,24 @@ def authenticate():
     # The requests property contains the values property. The value property contains
     # data from both requests.args and requests.form. 
     
-    username = request.values['username'] 
-    password = request.values['password']
-
-    if username == "dn":
-        if password == "1738":
-            #response to a person who has the right credentials and logs in
-            return render_template('response.html', username=username)
-        else:
-            return render_template('login.html', error='password')
+    if request.method == "GET":
+        return disp_loginpage()
     else:
-        return render_template('login.html', error='username')
+        username = request.values['username'] 
+        password = request.values['password']
+
+        if username == "dn":
+            if password == "1738":
+                session["username"] = username
+                #response to a person who has the right credentials and logs in
+                return render_template('response.html', username=username)
+            else:
+                return render_template('login.html', error='password')
+        else:
+            return render_template('login.html', error='username')
 
 if __name__ == "__main__": #false if this file imported as module
+    app.secret_key = urandom(32) # randomized secret key
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True
     app.run()
