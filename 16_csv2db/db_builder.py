9554598ff4d6b6eb3226b796c9548ac78c559342
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-21 09:26:16 -0400

    inital commit with one database done

---
+++ 16_csv2db/db_builder.py
@@ -0,0 +1,34 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev  
+#skeleton/stub :: SQLITE3 BASICS
+#Dec 2020 -- The Time of the Rona
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+#==========================================================
+
+c.execute("CREATE TABLE roster(name TEXT, age INTEGER, userid INTEGER)")
+
+with open('students.csv', newline='') as studentcsv:
+    toRead = csv.DictReader(studentcsv)
+    for student in toRead:
+        c.execute("INSERT INTO roster VALUES (\""+student['name']+"\", "+student['age']+", "+student['id']+")")
+
+
+
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
+
+
