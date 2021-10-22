157e8e55861ad104d850f44dad62da9bd663bb35
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-22 09:02:36 -0400

    added some notes

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -14,20 +14,23 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
-c.execute("CREATE TABLE roster(name TEXT, age INTEGER, userid INTEGER)")
+c.execute("CREATE TABLE roster(name TEXT, age INTEGER, userid INTEGER)") # create student table in database
 
 with open('students.csv', newline='') as studentcsv:
     toRead = csv.DictReader(studentcsv)
     for student in toRead:
         c.execute("INSERT INTO roster VALUES (\""+student['name']+"\", "+student['age']+", "+student['id']+")")
-
+        # for each row in csv, insert the row data into the data (same order as original table)
+        # also first row gets ignored as they are the headers
 c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)")
 
-with open('courses.csv', newline='') as coursescsv:
+with open('courses.csv', newline='') as coursescsv: # create course table in database
     toRead = csv.DictReader(coursescsv)
     for course in toRead:
         c.execute("INSERT INTO courses VALUES (\""+course['code']+"\", "+course['mark']+", "+course['id']+")")
-
+        # for each row in csv, insert the row data into the data (same order as original table)
+        # also first row gets ignored as they are the headers
+        
 command = ""          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
 
